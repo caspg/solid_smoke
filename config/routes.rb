10b2bc19d2d4fd6c26979e2b8f2a2feb42bc9701
caspg <kacper.golinski@gmail.com>
2015-01-27 19:01:14 +0100

    customize routings

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
 
   resources :movies, except: [:edit, :update, :destroy]
 
-  devise_for :users
-  root  'static_pages#home'
+  devise_for :users, :skip => [:sessions]
+  as :user do
+    get    "/login"  => "devise/sessions#new",     :as => :new_user_session
+    post   "/login"  => "devise/sessions#create",  :as => :user_session
+    delete "/logout" => "devise/sessions#destroy", :as => :destroy_user_session
+  end
+
+  root  "static_pages#home"
 
 end
