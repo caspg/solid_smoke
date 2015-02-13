41140f3a8115e74c083374d402c234789746d47c
caspg <kacper.golinski@gmail.com>
2015-02-13 13:40:40 +0100

    allow user to sign-in using username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,16 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :reviews, dependent: :destroy
+  attr_accessor :login
+  validates :username, presence: true, uniqueness: { case_sensitive: false },
+            format: { with: /\A[a-zA-Z0-9]+\Z/, message: "only allows letters and numbers" }
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions.to_h).first
+    end
+  end
 end
