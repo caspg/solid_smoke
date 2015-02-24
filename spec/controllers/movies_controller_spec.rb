b01676ca38f2bb97a97f1a814a0411ce524f2954
caspg <kacper.golinski@gmail.com>
2015-02-24 15:31:00 +0100

    empty movies_controller_spec

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -1,44 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe MoviesController, :type => :controller do
-  describe "GET index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
 
-  describe "GET new" do
-    before(:each) { login_user }
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET show" do
-    # This doesn't work and display this message:
-    #
-    # """ Failure/Error: get :show
-    # ActionController::UrlGenerationError:
-    # No route matches {:action=>"show", :controller=>"movies"} """
-    #
-    # rake routes: 
-    # movie GET    /movies/:id(.:format)          movies#show
-    # -----------------------------------------------------------
-    # xit "returns http success" do
-    #   get :show
-    #   expect(response).to have_http_status(:success)
-    # end
-  end
-
-  describe "GET search_info" do
-    
-    context "when user is unlogged" do
-      it "redirect user to login page" do
-        get :search_info
-        expect(response).to redirect_to(new_user_session_path)
-      end
-    end
-  end
 end
